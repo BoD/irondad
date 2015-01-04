837278ee3259d150338dfde2abc23fca6ea15892
BoD <BoD@JRAF.org>
2015-01-04 15:33:40 +0100

    Fixed lack of trimming.

--- src/main/java/org/jraf/irondad/handler/CommandHandler.java
+++ src/main/java/org/jraf/irondad/handler/CommandHandler.java
@@ -13,12 +13,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see
  * <http://www.gnu.org/licenses/>.
@@ -36,6 +36,6 @@ public abstract class CommandHandler extends BaseHandler {
     @Override
     public boolean isMessageHandled(String channel, String fromNickname, String text, List<String> textAsList, Message message, HandlerContext handlerContext) {
         String command = getCommand();
-        return text.trim().toLowerCase(Locale.getDefault()).startsWith(command);
+        return text.trim().toLowerCase(Locale.getDefault()).startsWith(command.toLowerCase(Locale.getDefault()));
     }
 }
--- src/main/java/org/jraf/irondad/handler/HandlerManager.java
+++ src/main/java/org/jraf/irondad/handler/HandlerManager.java
@@ -13,12 +13,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see
  * <http://www.gnu.org/licenses/>.
@@ -109,7 +109,7 @@ public class HandlerManager {
 
     public void handle(String channel, String fromNickname, String text, Message message) {
         String chanOrNick = channel == null ? fromNickname : channel;
-        List<String> textAsList = Arrays.asList(text.split("\\s+"));
+        List<String> textAsList = Arrays.asList(text.trim().split("\\s+"));
         if (channel == null) {
             // Handle privmsgs
             for (Handler handler : mPrivmsgHandlerContexts.keySet()) {
