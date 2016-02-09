5874c1d2d0e74d8be8a2a20197420a0196c3751f
BoD <BoD@JRAF.org>
2016-02-09 10:10:10 +0100

    Added title to reply.

--- src/main/java/org/jraf/irondad/handler/feed/FeedHandler.java
+++ src/main/java/org/jraf/irondad/handler/feed/FeedHandler.java
@@ -13,12 +13,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see
  * <http://www.gnu.org/licenses/>.
@@ -73,7 +73,8 @@ public class FeedHandler extends BaseHandler {
             if (Config.LOGD) Log.d(TAG, "handleChannelMessage Could not get link for this entry");
             return;
         }
-        connection.send(Command.PRIVMSG, channel, entryLink);
+        String entryTitle = latestEntry.getTitle();
+        connection.send(Command.PRIVMSG, channel, entryTitle + " " + entryLink);
     }
 
     private SyndEntry getLatestEntry(String urlStr) {
