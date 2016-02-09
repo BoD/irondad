1f0072cbb8594b2d93353d753aa9cb7c916d24c5
BoD <BoD@JRAF.org>
2016-02-09 10:09:36 +0100

    Updated pattern.

--- src/main/java/org/jraf/irondad/handler/srm/SrmHandler.java
+++ src/main/java/org/jraf/irondad/handler/srm/SrmHandler.java
@@ -38,7 +38,7 @@ import org.jraf.irondad.protocol.Message;
 
 public class SrmHandler extends BaseHandler {
     private static final String NICKNAME = "srm";
-    private static final Pattern PATTERN_OSEF = Pattern.compile("(.*note\\s*4.*)|(.*n\\s*4.*)|(.*nexus\\s*6.*)|(.*n\\s*6.*)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern PATTERN_OSEF = Pattern.compile("(.*note\\s*4.*)|(.*n\\s*4.*)|(.*nexus.*)|(.*n\\s*6.*)", Pattern.CASE_INSENSITIVE);
 
 
     @Override
@@ -52,7 +52,7 @@ public class SrmHandler extends BaseHandler {
             HandlerContext handlerContext) throws Exception {
         if (!fromNickname.toLowerCase(Locale.US).contains(NICKNAME.toLowerCase(Locale.US))) return;
         if (osef(text)) {
-            connection.send(Command.PRIVMSG, new String[] { channel, fromNickname + ": osef de ton Note 4 et/ou de ton Nexus 6" });
+            connection.send(Command.PRIVMSG, new String[] { channel, fromNickname + ": osef de ton Note 4 et/ou de ton Nexus 6[P]" });
         }
     }
 
