b8186a61513e4d676cc162dfa053bb6d894388ee
BoD <BoD@JRAF.org>
2016-06-11 21:54:24 +0200

    That's what she said

---
+++ src/main/java/org/jraf/irondad/handler/thatswhatshesaid/ThatsWhatSheSaidHandler.java
@@ -0,0 +1,49 @@
+/*
+ * This source is part of the
+ *      _____  ___   ____
+ *  __ / / _ \/ _ | / __/___  _______ _
+ * / // / , _/ __ |/ _/_/ _ \/ __/ _ `/
+ * \___/_/|_/_/ |_/_/ (_)___/_/  \_, /
+ *                              /___/
+ * repository.
+ *
+ * Copyright (C) 2014 Benoit 'BoD' Lubek (BoD@JRAF.org)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package org.jraf.irondad.handler.thatswhatshesaid;
+
+import java.util.List;
+
+import org.jraf.irondad.handler.BaseHandler;
+import org.jraf.irondad.handler.HandlerContext;
+import org.jraf.irondad.protocol.Command;
+import org.jraf.irondad.protocol.Connection;
+import org.jraf.irondad.protocol.Message;
+
+public class ThatsWhatSheSaidHandler extends BaseHandler {
+    private static final float PROBABILITY = 1 / 1000f;
+
+    @Override
+    public boolean isMessageHandled(String channel, String fromNickname, String text, List<String> textAsList, Message message, HandlerContext handlerContext) {
+        return channel != null && Math.random() > PROBABILITY;
+    }
+
+    @Override
+    protected void handleChannelMessage(Connection connection, String channel, String fromNickname, String text, List<String> textAsList, Message message,
+            HandlerContext handlerContext) throws Exception {
+        connection.send(Command.PRIVMSG, new String[] { channel, "That's what she said" });
+    }
+}
